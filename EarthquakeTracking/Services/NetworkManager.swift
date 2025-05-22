1104e023a2ddeab8ded925877458014869a69c3f
asarogluahmethakan <asarogluahmethakan@gmail.com>
2025-05-22 16:29:01 +0300

    comment satırları düzeltildi, kaldırıldı

--- EarthquakeTracking/Services/NetworkManager.swift
+++ EarthquakeTracking/Services/NetworkManager.swift
@@ -1,18 +1,15 @@
 import Foundation
 
-// NetworkManager için delegate protokolü
 protocol NetworkManagerDelegate: AnyObject {
     func didUpdateEarthquakes(_ earthquakes: [Earthquake])
 }
 
 class NetworkManager {
-    // Bildirim ismi
+
     static let earthquakesUpdatedNotification = Notification.Name("earthquakesUpdatedNotification")
     
-    // Deprem verileri
     private(set) var earthquakes: [Earthquake] = []
     
-    // Delegate referansı
     weak var delegate: NetworkManagerDelegate?
     
     func loadData() {
@@ -21,10 +18,8 @@ class NetworkManager {
                 let fetchedEarthquakes = data ?? []
                 self?.earthquakes = fetchedEarthquakes
                 
-                // Delegate ile bildir
                 self?.delegate?.didUpdateEarthquakes(fetchedEarthquakes)
                 
-                // Notification ile bildir
                 NotificationCenter.default.post(
                     name: NetworkManager.earthquakesUpdatedNotification,
                     object: self,
